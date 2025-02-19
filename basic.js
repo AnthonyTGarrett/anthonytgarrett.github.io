21a4636dcc913ad408b2c24ab3c530d1e059ecb7
Tony Garrett <tony.garrett@gmail.com>
2025-02-19 18:09:06 -0500

    Final form

--- basic.js
+++
@@ -1,5 +0,0 @@
-const height = document.querySelector('.section-two-right').offsetHeight;
-
-let rightSideSection = document.querySelector('.test1');
-console.log(rightSideSection);
-rightSideSection.setAttribute('style', `height: ${height}`);
