1acc3859e87d6c66cff36541d3fc30a5127851a9
Anthony Garrett <tony.garrett@gmail.com>
2025-05-07 20:26:30 -0400

    Just need to finish the contact section of the mobile site layout

--- js/script.js
+++ js/script.js
@@ -21,3 +21,29 @@ document.addEventListener('DOMContentLoaded', event => {
     window.Location.href = event.target.href;
   });
 });
+
+function changeCss() {
+  let headerElement = document.querySelector('.main-nav');
+  const headerLinks = document.querySelectorAll('.link');
+
+  if (this.scrollY > 80 && window.innerWidth > 768) {
+    headerElement.style.position = 'Fixed';
+    headerElement.style.backgroundColor = 'rgba(255,255,255,0.9)';
+    headerElement.style.paddingTop = '1rem';
+    headerElement.style.height = '50px';
+
+    for (let i = 0; i < headerLinks.length; i++) {
+      headerLinks[i].style.color = '#41413f';
+    }
+  } else if (this.scrollY < 80 && window.innerWidth > 768) {
+    headerElement.style.backgroundColor = 'transparent';
+    headerElement.style.paddingTop = '3rem';
+    headerElement.style.height = '55px';
+
+    for (let i = 0; i < headerLinks.length; i++) {
+      headerLinks[i].style.color = '#d3d3d3';
+    }
+  }
+}
+
+window.addEventListener('scroll', changeCss, false);
