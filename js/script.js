6ddf2e660e7410d8f63e66aab0f9788cd14d952c
Tony Garrett <tony.garrett@gmail.com>
2025-06-08 18:32:40 -0400

    changed main-nav to fixed position

--- js/script.js
+++ js/script.js
@@ -22,28 +22,20 @@ document.addEventListener('DOMContentLoaded', event => {
   });
 });
 
-function changeCss() {
-  let headerElement = document.querySelector('.main-nav');
-  const headerLinks = document.querySelectorAll('.link');
+// function changeCss() {
+//   let headerElement = document.querySelector('.main-nav');
+//   const headerLinks = document.querySelectorAll('.link');
 
-  if (this.scrollY > 80 && window.innerWidth > 768) {
-    headerElement.style.position = 'Fixed';
-    headerElement.style.backgroundColor = 'rgba(255,255,255,0.9)';
-    headerElement.style.paddingTop = '1rem';
-    headerElement.style.height = '50px';
+//   if (this.scrollY > 80 && window.innerWidth > 768) {
+//     headerElement.style.position = 'Fixed';
+//     headerElement.style.backgroundColor = 'rgba(255,255,255,0.9)';
+//     headerElement.style.paddingTop = '1rem';
+//     headerElement.style.height = '50px';
+//   } else if (this.scrollY < 80 && window.innerWidth > 768) {
+//     headerElement.style.backgroundColor = 'transparent';
+//     headerElement.style.paddingTop = '3rem';
+//     headerElement.style.height = '55px';
+//   }
+// }
 
-    // for (let i = 0; i < headerLinks.length; i++) {
-    //   headerLinks[i].style.color = '#41413f';
-    // }
-  } else if (this.scrollY < 80 && window.innerWidth > 768) {
-    headerElement.style.backgroundColor = 'transparent';
-    headerElement.style.paddingTop = '3rem';
-    headerElement.style.height = '55px';
-
-    // for (let i = 0; i < headerLinks.length; i++) {
-    //   headerLinks[i].style.color = '#d3d3d3';
-    // }
-  }
-}
-
-window.addEventListener('scroll', changeCss, false);
+// window.addEventListener('scroll', changeCss, false);
